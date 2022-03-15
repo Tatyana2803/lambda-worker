bb2f7332bbb427362c1153dd892f0f09c48b0fbe
Кондакова Татьяна <rotaru2803@rambler.ru>
2022-03-15 17:39:16 +0500

    add all files

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        OnTaskDoneListener linester = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+
+        Worker worker = new Worker(linester, errorListener);
+
+        worker.start();
+
+    }
+}
